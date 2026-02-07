57128c02ec3d76518d3ac9f2d057fa913d844e12
mncedcyR <thabekooloomncedcy@gmail.com>
2026-02-07 10:12:05 +0200

    implement rest data provider

--- src/pages/subjects/list.tsx
+++ src/pages/subjects/list.tsx
@@ -3,63 +3,73 @@ import {ListView} from "@/components/refine-ui/views/list-view.tsx";
 import {Breadcrumb} from "@/components/refine-ui/layout/breadcrumb.tsx";
 import {Search} from "lucide-react";
 import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select.tsx";
-import {DEPARTMENT, DEPARTMENT_OPTIONS} from "@/constants";
+import {DEPARTMENT_OPTIONS} from "@/constants";
 import {CreateButton} from "@/components/refine-ui/buttons/create.tsx";
 import {DataTable} from "@/components/refine-ui/data-table/data-table.tsx";
 import {useTable} from "@refinedev/react-table";
 import {Subject} from "@/types";
 import {ColumnDef} from "@tanstack/react-table";
 import {Badge} from "@/components/ui/badge.tsx";
+import {Input} from "@/components/ui/input.tsx";
 
 const SubjectList = () => {
 
     const [searchQuery, setSearchQuery] = useState('');
     const [selectedDepartment, setSelectedDepartment] = useState('all');
 
-    const departmentFilters = selectedDepartment === 'all' ? [] :
-        [{field: 'department', operator: 'eq' as const, value: selectedDepartment}];
+    const departmentFilters =
+        selectedDepartment === 'all' ? [] :
+        [{
+        field: 'department', operator: 'eq' as const, value: selectedDepartment}];
     const searchFilters =
         searchQuery ? [{field: 'name', operator: 'containsString' as const, value: searchQuery}] : [];
 
+    // @ts-ignore
     const subjectTable = useTable<Subject>({
         columns:useMemo<ColumnDef<Subject>[]>(() =>[
             {
-                id:'code',
-                size:100,
-                header:() => <p className="column-title ml-2">Code</p>,
-                accessorKey:'code',
-                cell:({getValue})=> <Badge>{getValue<string>()}</Badge>
+                id: "code",
+                accessorKey: "code",
+                size: 100,
+                header: () => <p className="column-title ml-2">Code</p>,
+                cell: ({ getValue }) => <Badge>{getValue<string>()}</Badge>,
             },
             {
-                id:'name',
-                size:200,
-                header:() => <p className="column-title">Name</p>,
-                accessorKey:'name',
-                cell:({getValue})=>
-                    <span className="text-foreground">{getValue<string>()}</span>,
-                filterFn: 'includesString'
+                id: "name",
+                accessorKey: "name",
+                size: 200,
+                header: () => <p className="column-title">Name</p>,
+                cell: ({ getValue }) => (
+                    <span className="text-foreground">{getValue<string>()}</span>
+                ),
+                filterFn: "includesString",
             },
             {
-                id:'department',
-                size:150,
-                header:() => <p className="column-title">Department</p>,
-                accessorKey:'department',
-                cell:({getValue})=> <Badge variant="secondary">{getValue<string>()}</Badge>,
+                id: "department",
+                accessorKey: "department.name",
+                size: 150,
+                header: () => <p className="column-title">Department</p>,
+                cell: ({ getValue }) => (
+                    <Badge variant="secondary">{getValue<string>()}</Badge>
+                ),
             },
             {
-                id:'description',
-                size:200,
-                header:() => <p className="column-title">Description</p>,
-                accessorKey:'description',
-                cell:({getValue})=>
-                    <span className="truncate line-clamp-2">{getValue<string>()}</span>,
-            }
+                id: "description",
+                accessorKey: "description",
+                size: 300,
+                header: () => <p className="column-title">Description</p>,
+                cell: ({ getValue }) => {
+                    const value = getValue<string>();
+                    return <span className="truncate line-clamp-2">{value ?? "No description"}</span>;
+                },
+            },
         ],[]),
+
         refineCoreProps: {
             resource:'subjects',
             pagination:{pageSize: 10,mode:'server'},
             filters:{
-                permanent: [...searchFilters,...departmentFilters],
+                permanent: [...departmentFilters, ...searchFilters],
             },
             sorters:{
                 initial:[
@@ -79,13 +89,14 @@ const SubjectList = () => {
 
                 <div className="actions-row">
                     <div className="search-field">
-                        <Search className="search-icon"/>
-                        <input
+                        <Search className="search-icon" />
+                        <Input
                             type="text"
                             placeholder="Search by name..."
                             className="pl-10 w-full"
                             value={searchQuery}
-                        onChange={(e) => setSearchQuery(e.target.value)}/>
+                            onChange={(event) => setSearchQuery(event.target.value)}
+                        />
                     </div>
                     <div className="flex gap-2  w-full sm:w-auto">
                         <Select value={selectedDepartment}
